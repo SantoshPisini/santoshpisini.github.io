8056bf316a423005b50279667de0ef3ef3fcd8e6
Santosh Pisini <>
2023-05-30 22:06:13 +0530

    Updated CSS, Added routing and links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RedirectComponent } from './components/redirect/redirect.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'resume',
+    component: RedirectComponent,
+    data: { key: 'resume' },
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
