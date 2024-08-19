8e3388ab043e66cbc67ad956d30eec5d88e1eea5
Santosh Pisini <santoshsrisai36@gmail.com>
2024-08-19 21:49:46 +0530

    Build fix - 3

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isGitHubPages = process.env.DEPLOY_TARGET === 'github-pages';
+
 const nextConfig = {
+  assetPrefix: isGitHubPages ? 'https://SantoshPisini/santoshpisini.github.io' : '',
   output: "export",
   images: {
     unoptimized: true,
